7312867c02b8652db9a97627e816f3143c250406
Maxim Korzenok <maximka00007777@gmail.com>
2021-08-10 01:48:04 +0300

    add pow function

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const output_result_element = document.querySelector(".result .value");
 const theme_elements = document.querySelectorAll(".theme");
 
 let memValue = 0;
+const OPERATORS = ["+", "-", "*", "/"];
+const POWER = "POWER(";
 
 let data = {
   operation: [],
@@ -138,6 +140,12 @@ let calc_buttons = [
     formula: false,
     type: "save",
   },
+  {
+    name: "powY",
+    symbol: "xy",
+    formula: POWER,
+    type: "math_function",
+  },
 ];
 
 buttonPanel_element.addEventListener("click", (event) => {
@@ -146,6 +154,7 @@ buttonPanel_element.addEventListener("click", (event) => {
   if (!event.target.closest("button")) return;
   calc_buttons.forEach((button) => {
     if (button.symbol == element) calculator(button);
+    else if (button.name == event.target.id) calculator(button);
   });
 });
 
@@ -166,9 +175,7 @@ function calculator(button) {
       data.operation.pop();
       data.formula.pop();
     }
-  } else if (button.type == "calculate")
-    updateOutputResult(eval(data.formula.join("")));
-  else if (button.type == "save") {
+  } else if (button.type == "save") {
     if (button.symbol == "m+")
       memValue += parseInt(output_result_element.innerHTML, 10);
     else if (button.symbol == "m-")
@@ -176,9 +183,74 @@ function calculator(button) {
     else if (button.symbol == "mr") updateOutputResult(memValue);
     else if (button.symbol == "mc") memValue = 0;
     console.log(memValue);
+  } else if (button.type == "math_function") {
+    let symbol, formula;
+    if (button.name == "powY") {
+      symbol = "^(";
+      formula = button.formula;
+      data.operation.push(symbol);
+      data.formula.push(formula);
+    }
+  } else if (button.type == "calculate") {
+    let formula_str = data.formula.join("");
+
+    let POWER_SEARCH_RESULT = search(data.formula, POWER);
+
+    const BASES = powerBaseGetter(data.formula, POWER_SEARCH_RESULT);
+    BASES.forEach((base) => {
+      let toReplace = base + POWER;
+      console.log(base);
+      let replacement = "Math.pow(" + base + ",";
+      console.log(replacement);
+
+      formula_str = formula_str.replace(toReplace, replacement);
+      console.log(formula_str, "form");
+    });
+    console.log(data.formula);
+    updateOutputResult(eval(formula_str));
+    return;
   }
 
   updateOutputOperation(data.operation.join(""));
+}
+
+function powerBaseGetter(formula, POWER_SEARCH_RESULT) {
+  let powers_bases = [];
+
+  POWER_SEARCH_RESULT.forEach((power_index) => {
+    let base = [];
+
+    let parentheses_count = 0;
+
+    let previous_index = power_index - 1;
+
+    while (previous_index >= 0) {
+      if (formula[previous_index] == "(") parentheses_count--;
+      if (formula[previous_index] == ")") parentheses_count++;
+
+      let is_operator = false;
+      OPERATORS.forEach((OPERATOR) => {
+        if (formula[previous_index] == OPERATOR) is_operator = true;
+      });
+
+      let is_power = formula[previous_index] == POWER;
+
+      if ((is_operator && parentheses_count == 0) || is_power) break;
+
+      base.unshift(formula[previous_index]);
+      previous_index--;
+    }
+    powers_bases.push(base.join(""));
+  });
+  return powers_bases;
+}
+
+function search(array, key) {
+  let result = [];
+  array.forEach((element, index) => {
+    if (element == key) result.push(index);
+  });
+  return result;
 }
 
 function updateOutputOperation(operation) {
@@ -187,8 +259,6 @@ function updateOutputOperation(operation) {
 function updateOutputResult(result) {
   output_result_element.innerHTML = result;
 }
-
-/* Theme manager */
 
 function themeSwitch(color) {
   theme_elements.forEach((item) => {
